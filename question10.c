aeeae37cb6bfbc4e91b8458e8f96c0f5d38daa62
Md Rohit <108532993+mdrohit22@users.noreply.github.com>
2022-07-23 19:17:55 +0530

    Add files via upload

---
+++ question10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int number = 5, i = 1;
+    while(i<=10)
+    {
+    printf("Table of 5 is %dX%d = %d\n", 5, i , 5*i);
+    i++;
+
+    }
+    
+    return 0;
+}
